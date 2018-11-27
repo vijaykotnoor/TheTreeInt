a3a3323d4828179e0cb7aff86822f64b0a375e6b
vijaykotnoor <vijay@192.168.0.17>
2018-11-26 21:35:38 -0600

    Adding segement tree

---
+++ TheTreeInt/src/segmentTree.cpp
@@ -0,0 +1,18 @@
+/*
+ * segmentTree.cpp
+ *
+ *  Created on: 26-Nov-2018
+ *      Author: vijay
+ */
+
+#include "segmentTree.h"
+
+segmentTree::segmentTree() {
+	// TODO Auto-generated constructor stub
+
+}
+
+segmentTree::~segmentTree() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ TheTreeInt/src/segmentTree.h
@@ -0,0 +1,17 @@
+/*
+ * segmentTree.h
+ *
+ *  Created on: 26-Nov-2018
+ *      Author: vijay
+ */
+
+#ifndef SEGMENTTREE_H_
+#define SEGMENTTREE_H_
+
+class segmentTree {
+public:
+	segmentTree();
+	virtual ~segmentTree();
+};
+
+#endif /* SEGMENTTREE_H_ */
